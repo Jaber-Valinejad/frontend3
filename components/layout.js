4d64b09ccd2ac66b3f4b8351b44d36ad696fa96e
Jaber Valinejad <66491004+Jaber-Valinejad@users.noreply.github.com>
2025-04-22 23:12:23 -0400

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,10 @@
-import styles from '../styles/layout.module.c
+import styles from '../styles/layout.module.css'
 
 export default function Layout(props) {
   return (
-    <div style={{
-      backgroundImage: "url('/background.png')",
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      minHeight: "100vh"
-    }}>
-      <h1 style={{ padding: '1rem', backgroundColor: 'rgba(255,255,255,0.8)' }}>To Do</h1>
+    <div className={styles.layout}>
+      <h1 className={styles.title}>To Do</h1>
       {props.children}
     </div>
-  );
+  )
 }
